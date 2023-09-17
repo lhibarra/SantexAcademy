5860249e6052c59e65cb9d2098b2896ad1d99b09
Semper Evincere <spereyra.jus@gmail.com>
2023-09-17 19:10:30 -0300

    path en expansion.module corregido

--- front/src/app/components/dashboard/encuesta/expansion/expansion.module.ts
+++ front/src/app/components/dashboard/encuesta/expansion/expansion.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CompartidaModule } from 'src/app/component/compartida/compartida.module';
+import { CompartidaModule } from '../../../compartida/compartida.module';
 
 
 @NgModule({
