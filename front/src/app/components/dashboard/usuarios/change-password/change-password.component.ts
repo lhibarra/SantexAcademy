ea980aa1aae02ca92074d5ce9369b8f271f36bc1
Semper Evincere <spereyra.jus@gmail.com>
2023-10-01 10:21:55 -0300

    se refactoriza el codigo del cambio de pass para que tome el userId actual y mensajes de exito o rechazo con snackbar

--- front/src/app/components/dashboard/usuarios/change-password/change-password.component.ts
+++ front/src/app/components/dashboard/usuarios/change-password/change-password.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
-import { catchError } from 'rxjs/operators';
-import { AuthService } from 'src/app/services/auth.service';
 import { UserService } from 'src/app/services/usuario.service';
 
 @Component({
@@ -12,16 +11,16 @@ import { UserService } from 'src/app/services/usuario.service';
 })
 export class ChangePasswordComponent implements OnInit {
   passwordForm: FormGroup;
-  
-  constructor(private fb: FormBuilder, 
-              private authService: AuthService,
-              private userService: UserService,
-              private router: Router) {
+
+  constructor(private fb: FormBuilder,
+    private snackBar: MatSnackBar,
+    private userService: UserService,
+    private router: Router) {
     this.passwordForm = this.fb.group({
       currentPassword: ['', Validators.required],
       newPassword: ['', Validators.required],
     });
-   }
+  }
 
   ngOnInit(): void {
   }
@@ -29,8 +28,29 @@ export class ChangePasswordComponent implements OnInit {
   changePassword() {
     if (this.passwordForm.valid) {
       this.userService.changePassword(this.passwordForm.value)
-        .then(() => this.router.navigate(['dashboard']) )
-        .catch(catchError)
+        .then(() => {
+          const snackBarRef = this.snackBar.open("La contraseña fue actualizada con éxito!", "", {
+            duration: 1000,
+            horizontalPosition: "center",
+            verticalPosition: "bottom"
+          });
+  
+          snackBarRef.afterDismissed().subscribe(() => {
+            this.router.navigate(['dashboard']);
+          });
+        })
+        .catch(() => {
+          const snackBarRef = this.snackBar.open(`No pudo actualizarse la contraseña`, "", {
+            duration: 1500,
+            horizontalPosition: "center",
+            verticalPosition: "bottom",
+            panelClass: ['custom-snackbar']
+          });
+  
+          snackBarRef.afterDismissed().subscribe(() => {
+            this.router.navigate(['dashboard']);
+          });
+        })
     }
   }
 }
